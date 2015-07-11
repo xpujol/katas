d7a2b74572708b673a13d51d8df64d1c8792eb6b
Xevi Pujol <xpujol84@gmail.com>
2015-07-11 17:44:21 +0200

    Solved 4: template strings - String.raw

--- katas/es6/language/template-strings/raw.js
+++ katas/es6/language/template-strings/raw.js
@@ -5,14 +5,14 @@ describe('on tagged template strings you can use String.raw', function() {
   
   it('the `raw` property accesses the string as it was entered', function() {
     function firstChar(strings) {
-      return strings;
+      return strings.raw;
     }
     assert.equal(firstChar`\n`, '\\n');
   });
 
   it('`raw` can access the backslash of a line-break', function() {
     function firstCharEntered(strings) {
-      var lineBreak = strings.raw[0];
+      var lineBreak = strings.raw[0][0];
       return lineBreak;
     }
     assert.equal(firstCharEntered`\n`, '\\');
@@ -21,17 +21,17 @@ describe('on tagged template strings you can use String.raw', function() {
   describe('`String.raw` as a static function', function(){
     
     it('concats the raw strings', function() {
-      var expected = '\n';
+      var expected = '\\n';
       assert.equal(String.raw`\n`, expected);
     });
     
     it('two raw-templates-string-backslashes equal two escaped backslashes', function() {
-      const TWO_BACKSLASHES = '\\';
+      const TWO_BACKSLASHES = '\\\\';
       assert.equal(String.raw`\\`, TWO_BACKSLASHES);
     });
     
     it('works on unicodes too', function() {
-      var smilie = '\u{1F600}';
+      var smilie = '\\u{1F600}';
       var actual = String.raw`\u{1F600}`;
       assert.equal(actual, smilie);
     });
