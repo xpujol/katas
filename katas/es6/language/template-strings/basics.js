f2e4daac297d93175cc68ce47837544897582427
Xevi Pujol <xpujol84@gmail.com>
2015-07-11 17:24:36 +0200

    Solved 1: template strings - basics

--- katas/es6/language/template-strings/basics.js
+++ katas/es6/language/template-strings/basics.js
@@ -5,18 +5,18 @@ describe('template string, are wrapped in backticks', function() {
   
   it('prints x into a string using ${x}', function() {
     var x = 42;
-    assert.equal(`x=${x}`, 'x=' + x);
+    assert.equal(`x=${x}`, 'x=42');
   });
   
   it('add two numbers inside the ${...}', function() {
     var x = 42;
     var y = 23;
-    assert.equal(`${ x } + $ { y }`, x+y);
+    assert.equal(`${ x + y }`, x+y);
   });
   
   it('get a function call result inside ${...}', function() {
     function getDomain(){ return document.domain }
-    assert.equal(`${ getDomain }`, 'tddbin.com');
+    assert.equal(`${ getDomain() }`, 'tddbin.com');
   });
   
 });
