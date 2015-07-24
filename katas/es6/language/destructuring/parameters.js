b31a56e4463374038e2b51f00e2f6f9d20b11fdc
Xevi Pujol <xpujol84@gmail.com>
2015-07-24 11:26:27 +0200

    Solved 14: destructuring - parameters

--- katas/es6/language/destructuring/parameters.js
+++ katas/es6/language/destructuring/parameters.js
@@ -5,7 +5,7 @@ describe('destructuring function parameters', () => {
 
   describe('destruct parameters', () => {
     it('multiple params from object', () => {
-      const fn = ({id}, {name}) => {
+      const fn = ({id, name}) => {
         assert.equal(id, 42);
         assert.equal(name, 'Wolfram');
       };
@@ -17,14 +17,14 @@ describe('destructuring function parameters', () => {
       const fn = ([{name}]) => {
         assert.equal(name, 'Alice');
       };
-      const users = [{name: 'nobody'}, {name: 'Alice', id: 42}];
+      const users = [{name: 'Alice'}, {name: 'Alice', id: 42}];
       fn(users);
     });
   });
 
   describe('default values', () => {
     it('for simple values', () => {
-      const fn = (id, name='Bobby') => {
+      const fn = (id, name='Bob') => {
         assert.strictEqual(id, 23);
         assert.strictEqual(name, 'Bob');
       };
@@ -33,14 +33,14 @@ describe('destructuring function parameters', () => {
     
     it('for a missing array value', () => {
       const defaultUser = {id: 23, name: 'Joe'};
-      const fn = ([user]) => {
+      const fn = ([user=defaultUser]) => {
         assert.deepEqual(user, defaultUser);
       };
       fn([]);
     });
     
     it('mix of parameter types', () => {
-      const fn = (id, [arr], {obj}) => {
+      const fn = (id=1, [arr=2], {obj=3}) => {
         assert.equal(id, 1);
         assert.equal(arr, 2);
         assert.equal(obj, 3);
