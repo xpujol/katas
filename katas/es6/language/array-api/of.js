204bba4f5840140ef1997b0a7c053cbc4bcc57ad
Xevi Pujol <xpujol84@gmail.com>
2015-08-06 14:04:04 +0200

    Solved 30: array - `Array.of` static method

--- katas/es6/language/array-api/of.js
+++ katas/es6/language/array-api/of.js
@@ -4,13 +4,13 @@
 describe('`Array.of` creates an array with the given arguments as elements', () => {
   
   it('dont mix it up with `Array(10)`, where the argument is the array length', () => {
-    const arr = Array(10);
+    const arr = Array.of(10);
     
     assert.deepEqual(arr, [10]);
   });
   
   it('puts all arguments into array elements', () => {
-    const arr = Array.of();
+    const arr = Array.of(1, 2);
     
     assert.deepEqual(arr, [1, 2]);
   });
@@ -18,7 +18,7 @@ describe('`Array.of` creates an array with the given arguments as elements', () 
   it('takes any kind and number of arguments', () => {
     const starter = [1, 2];
     const end = [3, '4'];
-    const arr = Array.of(...starter, ...end);
+    const arr = Array.of(starter, ...end);
     
     assert.deepEqual(arr, [[1, 2], 3, '4']);
   });
