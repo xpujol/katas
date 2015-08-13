5eb414783c3c569d75fefb3f2eb51e470ca9a278
Xevi Pujol <xpujol84@gmail.com>
2015-08-13 08:53:36 +0200

    Solved 35: Symbol.for - retreives or creates a runtime-wide symbol

--- katas/es6/language/symbol/for.js
+++ katas/es6/language/symbol/for.js
@@ -4,20 +4,20 @@
 describe('`Symbol.for` for registering Symbols globally', function() {
   
   it('creates a new symbol (check via `typeof`)', function() {
-    const symbolType = Symbol.for('symbol name');
+    const symbolType = typeof Symbol.for('symbol name');
     assert.equal(symbolType, 'symbol');
   });
   
   it('stores the symbol in a runtime-wide registry and retreives it from it', function() {
     const sym = Symbol.for('new symbol');
-    const sym1 = Symbol.for('new symbol1');
+    const sym1 = Symbol.for('new symbol');
     
     assert.equal(sym, sym1);
   });
   
   it('is different to `Symbol()` which creates a symbol every time and does not store it', function() {
     var globalSymbol = Symbol.for('new symbol');
-    var globalSymbol = Symbol('new symbol');
+    var localSymbol = Symbol('new symbol');
     
     assert.notEqual(globalSymbol, localSymbol);
   });
@@ -28,14 +28,14 @@ describe('`Symbol.for` for registering Symbols globally', function() {
     const symbolFromRegistry = Symbol.for('new symbol');
     
     it('also contains the key given to `Symbol.for()`', function() {
-      const description = localSymbol.toString;
+      const description = localSymbol.toString();
       assert.equal(description, 'Symbol(new symbol)');
     });
   
     describe('NOTE: the description of two different symbols', function() {
       it('might be the same', function() {
         const localDescription = localSymbol.toString();
-        const fromRegistryDescription = ''+symbolFromRegistry;
+        const fromRegistryDescription = symbolFromRegistry.toString();
         
         assert.equal(localDescription, fromRegistryDescription);
       });
